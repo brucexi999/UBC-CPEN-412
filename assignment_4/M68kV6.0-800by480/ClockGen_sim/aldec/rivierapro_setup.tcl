fc9ea8e8fea1bf6a1e691a65bd97490858775fc0
brucexi999 <91307696+brucexi999@users.noreply.github.com>
2023-03-12 14:52:25 -0700

    4b: 8 line 4 way associative cache done

--- assignment_4/M68kV6.0-800by480/ClockGen_sim/aldec/rivierapro_setup.tcl
+++ assignment_4/M68kV6.0-800by480/ClockGen_sim/aldec/rivierapro_setup.tcl
@@ -12,7 +12,7 @@
 # or its authorized distributors. Please refer to the applicable 
 # agreement for further details.
 
-# ACDS 18.1 625 win32 2023.03.03.09:55:37
+# ACDS 18.1 625 win32 2023.03.12.13:25:22
 # ----------------------------------------
 # Auto-generated simulation script rivierapro_setup.tcl
 # ----------------------------------------
--- assignment_4/M68kV6.0-800by480/ClockGen_sim/mentor/msim_setup.tcl
+++ assignment_4/M68kV6.0-800by480/ClockGen_sim/mentor/msim_setup.tcl
@@ -94,7 +94,7 @@
 # within the Quartus project, and generate a unified
 # script which supports all the Altera IP within the design.
 # ----------------------------------------
-# ACDS 18.1 625 win32 2023.03.03.09:55:37
+# ACDS 18.1 625 win32 2023.03.12.13:25:22
 
 # ----------------------------------------
 # Initialize variables
